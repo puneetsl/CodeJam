dcf24a67c0109e5ea1da535081a07f9550cd634c
Puneet Singh Ludu <pludu@buffalo.edu>
2015-11-04 00:02:44 -0500

    Added a new solution: Linked List Reversal

---
+++ src/main/java/me/puneetsingh/basics/misc/ReverseLinkedList.java
@@ -0,0 +1,48 @@
+package me.puneetsingh.basics.misc;
+
+
+import me.puneetsingh.utils.LinkedList.SimpleLinkedList;
+
+public class ReverseLinkedList {
+    private static void printLL(SimpleLinkedList node)
+    {
+        while(node.next!=null)
+        {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        System.out.println(node.val);
+    }
+    private static SimpleLinkedList createLL()
+    {
+        SimpleLinkedList head = new SimpleLinkedList(1);
+        SimpleLinkedList node = head;
+        for (int i = 2; i < 11; i++) {
+            node.next = new SimpleLinkedList(i);
+            node = node.next;
+        }
+        return head;
+    }
+    private static SimpleLinkedList reverseLL(SimpleLinkedList head)
+    {
+        SimpleLinkedList c = head.next;//current
+        SimpleLinkedList p = head;//previous
+        head.next=null;
+        SimpleLinkedList n;//next
+        while(c.next != null)
+        {
+            n = c.next;
+            c.next = p;
+            p=c;
+            c=n;
+        }
+        c.next = p;
+        return c;
+    }
+    public static void main(String[] args) {
+        SimpleLinkedList head = createLL();
+        printLL(head);
+        head = reverseLL(head);
+        printLL(head);
+    }
+}
---
+++ src/main/java/me/puneetsingh/utils/LinkedList/SimpleLinkedList.java
@@ -0,0 +1,10 @@
+package me.puneetsingh.utils.LinkedList;
+
+public class SimpleLinkedList {
+    public Integer val=0;
+    public SimpleLinkedList next = null;
+    public SimpleLinkedList(int val)
+    {
+        this.val = val;
+    }
+}
